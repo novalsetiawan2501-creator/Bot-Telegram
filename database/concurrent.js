dbbbf0132945c4d207222789d54b46f64e80b8c3
novalsetiawan2501-creator <novalsetiawan2501@gmail.com>
2026-07-26 11:55:18 +0700

    Add files via upload

---
+++ database/concurrent.js
@@ -0,0 +1,12 @@
+module.exports = {
+  1: { rate: 2, thread: 1 },
+  2: { rate: 32, thread: 2 },
+  3: { rate: 32, thread: 5 },
+  4: { rate: 62, thread: 5 },
+  5: { rate: 100, thread: 10 },
+  6: { rate: 150, thread: 15 },
+  7: { rate: 200, thread: 20 },
+  8: { rate: 250, thread: 25 },
+  9: { rate: 300, thread: 30 },
+  10: { rate: 500, thread: 50 }
+};
